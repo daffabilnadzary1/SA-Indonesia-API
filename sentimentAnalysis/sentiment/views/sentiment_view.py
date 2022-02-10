b73d9946af855b341d885c5a10c8228a01a7c0e6
Daffa Bilnadzary <daffa.bil.nadzary@365.ugm.ac.id>
2022-02-10 14:33:20 +0700

    adding the front-end using streamlit (on progress)

---
+++ sentimentAnalysis/sentiment/views/sentiment_view.py
@@ -0,0 +1,25 @@
+import streamlit as st
+# import sys 
+# sys.path.append("/sentiment/controller/")
+from .api import predicting
+
+st.title("My first app created in streamlit")
+
+
+with st.form("my_form"):
+    # st.write("Inside the form")
+    # slider_val = st.slider("Form slider")
+    # checkbox_val = st.checkbox("Form checkbox")
+
+    # # Every form must have a submit button.
+    # submitted = st.form_submit_button("Submit")
+    # if submitted:
+    #     st.write("slider", slider_val, "checkbox", checkbox_val)
+
+    text = st.text_input("Insert Your Sentiment Here")
+    bt_predict = st.form_submit_button("Predict")
+    if bt_predict:
+        prediction = predicting(text)
+        st.text("Results:")
+        st.json(prediction)
+        
